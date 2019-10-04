54f348a544ca6618ac277ad6288e754dc21bbc90
Artsem_Virshych <artsem_virshych@epam.com>
2019-10-04 15:08:32 +0300

    added functional for rds

--- aws_scheduler/static/js/search_dropdown.js
+++ aws_scheduler/static/js/search_dropdown.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-    $('.js-select2').select2();
+    $(".js-select2").select2();
+
+    const quickFormRds = document.querySelector(".quick-schedule__form_rds");
+
+    if (quickFormRds) {
+        let dbInstancearn, region;
+
+        $(".js-select2").on("select2:select", ({params}) => {
+            region = params.data.element.dataset.region;
+            dbInstancearn = params.data.element.dataset.dbinstancearn;
+        });
+
+        quickFormRds.addEventListener("submit", () => {
+            quickFormRds.action = `/rds/regions/${region}/instances/${dbInstancearn}/tags/add`;
+        });
+    }
 });

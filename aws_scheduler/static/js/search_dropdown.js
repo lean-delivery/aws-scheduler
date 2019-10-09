abd66c9340cd91a26245791d920cc126aedd4797
Artsem_Virshych <artsem_virshych@epam.com>
2019-10-09 13:31:40 +0300

    disabled button

--- aws_scheduler/static/js/search_dropdown.js
+++ aws_scheduler/static/js/search_dropdown.js
@@ -1,21 +1,38 @@
 $(document).ready(function() {
-    $(".js-select2").select2();
+    const select2 = $(".js-select2");
+    const quickFormRds = document.querySelector(".quick-schedule__form");
 
-    const quickFormRds = document.querySelector(".quick-schedule__form_rds") || null;
-    const buttonQuickFormRds = quickFormRds.querySelector(".button");
+    let region = null;
+    let dbInstancearn = null;
+    let countSelectBlock = 0;
 
-    if (quickFormRds) {
-        let dbInstancearn, region;
 
-        $(".js-select2").on("select2:select", ({params}) => {
-            region = params.data.element.dataset.region;
-            dbInstancearn = params.data.element.dataset.dbinstancearn;
 
-            buttonQuickFormRds.classList.remove("button__disabled");
-        });
+    const findParentByClassName = (element, tag) => {
+        while (element.tagName !== tag.toUpperCase()
+            ) {
+            if (element.tagName === "BODY") return null;
+            element = element.parentNode;
+        }
+        return element;
+    };
 
-        quickFormRds.addEventListener("submit", () => {
-            quickFormRds.action = `/rds/regions/${region}/instances/${dbInstancearn}/tags/add`;
-        });
-    }
+    $(".js-select2-instance").on("select2:select", ({params}) => {
+        region = params.data.element.dataset.region;
+        dbInstancearn = params.data.element.dataset.dbinstancearn;
+    });
+
+    quickFormRds.addEventListener("submit", () => {
+        quickFormRds.action = `/rds/regions/${region}/instances/${dbInstancearn}/tags/add`;
+    });
+
+    select2.select2().on("select2:select", () => {
+        countSelectBlock += 1;
+
+        if (select2.length === countSelectBlock) {
+            const form = findParentByClassName(target, "form");
+
+            form.querySelector(".button").classList.remove("button__disabled");
+        }
+    });
 });

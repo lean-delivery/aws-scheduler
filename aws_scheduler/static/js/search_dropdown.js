562f63da1198f591c2f8d778567651538c65af7f
Artsem_Virshych <artsem_virshych@epam.com>
2019-10-09 14:43:09 +0300

    refactoring code

--- aws_scheduler/static/js/search_dropdown.js
+++ aws_scheduler/static/js/search_dropdown.js
@@ -1,42 +1,29 @@
 $(document).ready(function() {
-    const select2 = $(".js-select2");
-    const quickFormRds = document.querySelector(".quick-schedule__form");
+    const quickForm = document.querySelector(".quick-schedule__form");
 
     let region = null;
     let dbInstancearn = null;
-    let countSelectBlock = 0;
 
+    $(".js-select2").select2();
 
+    $(".select2-form").on("select2:select", (evt) => {
+        const { currentTarget, params, target } = evt;
+        const arrayElementSelect = [...currentTarget.querySelectorAll(".js-select2")];
+        const isSelected = arrayElementSelect.every(item => item.selectedIndex !== 0);
 
-    const findParentByClassName = (element, tag) => {
-        while (element.tagName !== tag.toUpperCase()
-            ) {
-            if (element.tagName === "BODY") return null;
-            element = element.parentNode;
+        if (isSelected) {
+            currentTarget.querySelector(".button").classList.remove("button__disabled");
         }
-        return element;
-    };
 
-    $(".js-select2-instance").on("select2:select", ({params}) => {
-        region = params.data.element.dataset.region;
-        dbInstancearn = params.data.element.dataset.dbinstancearn;
+        if (target.classList.contains("js-select2-instance")) {
+            region = params.data.element.dataset.region;
+            dbInstancearn = params.data.element.dataset.dbinstancearn;
+        }
     });
 
-    quickFormRds.addEventListener("submit", () => {
-        quickFormRds.action = `/rds/regions/${region}/instances/${dbInstancearn}/tags/add`;
-    });
-
-    select2.select2().on("select2:select", ({target}) => {
-        const form = findParentByClassName(target, "form");
-        const countSelectForm = form.querySelectorAll(".js-select2").length;
-
-        countSelectBlock += 1;
-
-        if (countSelectForm === countSelectBlock) {
-            countSelectBlock = 0;
-            const form = findParentByClassName(target, "form");
-
-            form.querySelector(".button").classList.remove("button__disabled");
+    quickForm.addEventListener("submit", () => {
+        if (region && dbInstancearn) {
+            quickForm.action = `/rds/regions/${region}/instances/${dbInstancearn}/tags/add`;
         }
     });
 });
